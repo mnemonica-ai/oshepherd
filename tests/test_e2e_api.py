4ff05cff7eddbb0fed9467934b6ba739162c15fc
Raul Pino <pinox101@gmail.com>
2024-04-30 09:47:06 -0400

    tests added for chat completion basic cases using ollama python package and http requests

--- tests/test_e2e_api.py
+++ tests/test_e2e_api.py
@@ -1,5 +1,6 @@
 """
-Basic end to end tests, using an equivalent request as pointing to ollama api server in local:
+Basic end to end tests, using Ollama python package, and its equivalent http requests to an Ollama server in local.
+i.e.:
     curl -X POST -H "Content-Type: application/json" -L http://127.0.0.1:11434/api/generate/ -d '{
         "model": "mistral",
         "prompt":"Why is the sky blue?"
@@ -10,9 +11,10 @@ import json
 import requests
 import ollama
 from oshepherd.api.generate.models import GenerateResponse
+from oshepherd.api.chat.models import ChatResponse
 
 
-def test_basic_api_worker_queueing_using_ollama():
+def test_basic_generate_completion_using_ollama():
     params = {"model": "mistral", "prompt": "Why is the sky blue?"}
     client = ollama.Client(host="http://127.0.0.1:5001")
     ollama_res = client.generate(**params)
@@ -21,7 +23,7 @@ def test_basic_api_worker_queueing_using_ollama():
     assert ollama_res.response, "response should not be empty"
 
 
-def test_basic_api_worker_queueing_using_requests():
+def test_basic_generate_completion_using_requests():
     url = "http://127.0.0.1:5001/api/generate/"
     headers = {"Content-Type": "application/json"}
     data = {"model": "mistral", "prompt": "Why is the sky blue?"}
@@ -31,3 +33,30 @@ def test_basic_api_worker_queueing_using_requests():
     assert "error" not in response
     ollama_res = GenerateResponse(**response.json())
     assert ollama_res.response, "response should not be empty"
+
+
+def test_basic_chat_completion_using_ollama():
+    params = {
+        "model": "mistral",
+        "messages": [{"role": "user", "content": "why is the sky blue?"}],
+    }
+    client = ollama.Client(host="http://127.0.0.1:5001")
+    ollama_res = client.chat(**params)
+
+    ollama_res = ChatResponse(**ollama_res)
+    assert ollama_res.message.content, "response should not be empty"
+
+
+def test_basic_chat_completion_using_requests():
+    url = "http://127.0.0.1:5001/api/chat/"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "model": "mistral",
+        "messages": [{"role": "user", "content": "why is the sky blue?"}],
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    assert response.status_code == 200
+    assert "error" not in response
+    ollama_res = ChatResponse(**response.json())
+    assert ollama_res.message.content, "response should not be empty"
