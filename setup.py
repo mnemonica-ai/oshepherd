4ea89b052a8cea6981398c508902339b1b893d92
Carlos Betancourt Carrero <carbetacar@gmail.com>
2024-05-19 19:36:20 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="oshepherd",
-    version="0.0.6",
+    version="0.0.7",
     description="The Oshepherd guiding the Ollama(s) inference orchestration.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
