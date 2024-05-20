838ba932609063ca941b7fcc3faf8c418954d03b
Carlos Betancourt Carrero <carbetacar@gmail.com>
2024-05-20 19:33:26 +0200

    Use `model_fields` instead of `__fields__`

--- oshepherd/worker/app.py
+++ oshepherd/worker/app.py
@@ -32,7 +32,7 @@ def create_celery_app_for_flask(flask_app):
         RABBITMQ_URL=flask_app.config["RABBITMQ_URL"],
         REDIS_URL=flask_app.config["REDIS_URL"],
         RESULTS_EXPIRES=flask_app.config.get(
-            "RESULTS_EXPIRES", WorkerConfig.__fields__["RESULTS_EXPIRES"].default
+            "RESULTS_EXPIRES", WorkerConfig.model_fields["RESULTS_EXPIRES"].default
         ),
     )
 
