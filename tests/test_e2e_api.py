b6f94d9c21562c647ecf062423269fc423a31706
Raul Pino <pinox101@gmail.com>
2024-11-02 11:25:56 -0300

    constants for server data e2e tests

--- tests/test_e2e_api.py
+++ tests/test_e2e_api.py
@@ -14,10 +14,15 @@ from oshepherd.api.generate.models import GenerateResponse
 from oshepherd.api.chat.models import ChatResponse
 from oshepherd.api.embeddings.models import EmbeddingsResponse
 
+HOST = "http://127.0.0.1:5001"
+GENERATE_ENDPOINT = f"{HOST}/api/generate/"
+CHAT_ENDPOINT = f"{HOST}/api/chat/"
+EMBEDDINGS_ENDPOINT = f"{HOST}/api/embeddings/"
+
 
 def test_basic_generate_completion_using_ollama():
     params = {"model": "mistral", "prompt": "Why is the sky blue?"}
-    client = ollama.Client(host="http://127.0.0.1:5001")
+    client = ollama.Client(host=HOST)
     ollama_res = client.generate(**params)
 
     ollama_res = GenerateResponse(**ollama_res)
@@ -25,10 +30,9 @@ def test_basic_generate_completion_using_ollama():
 
 
 def test_basic_generate_completion_using_requests():
-    url = "http://127.0.0.1:5001/api/generate/"
     headers = {"Content-Type": "application/json"}
     data = {"model": "mistral", "prompt": "Why is the sky blue?"}
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = requests.post(GENERATE_ENDPOINT, headers=headers, data=json.dumps(data))
 
     assert response.status_code == 200
     assert "error" not in response
@@ -41,7 +45,7 @@ def test_basic_chat_completion_using_ollama():
         "model": "mistral",
         "messages": [{"role": "user", "content": "why is the sky blue?"}],
     }
-    client = ollama.Client(host="http://127.0.0.1:5001")
+    client = ollama.Client(host=HOST)
     ollama_res = client.chat(**params)
 
     ollama_res = ChatResponse(**ollama_res)
@@ -49,13 +53,12 @@ def test_basic_chat_completion_using_ollama():
 
 
 def test_basic_chat_completion_using_requests():
-    url = "http://127.0.0.1:5001/api/chat/"
     headers = {"Content-Type": "application/json"}
     data = {
         "model": "mistral",
         "messages": [{"role": "user", "content": "why is the sky blue?"}],
     }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = requests.post(CHAT_ENDPOINT, headers=headers, data=json.dumps(data))
 
     assert response.status_code == 200
     assert "error" not in response
@@ -64,13 +67,12 @@ def test_basic_chat_completion_using_requests():
 
 
 def test_basic_embeddings_using_requests():
-    url = "http://127.0.0.1:5001/api/embeddings/"
     headers = {"Content-Type": "application/json"}
     data = {
         "model": "mistral",
         "prompt": "The sky is blue because of rayleigh scattering",
     }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = requests.post(EMBEDDINGS_ENDPOINT, headers=headers, data=json.dumps(data))
 
     assert response.status_code == 200
     assert "error" not in response
@@ -83,7 +85,7 @@ def test_basic_embeddings_using_ollama():
         "model": "mistral",
         "prompt": "The sky is blue because of rayleigh scattering",
     }
-    client = ollama.Client(host="http://127.0.0.1:5001")
+    client = ollama.Client(host=HOST)
     ollama_res = client.embeddings(**params)
 
     ollama_res = EmbeddingsResponse(**ollama_res)
