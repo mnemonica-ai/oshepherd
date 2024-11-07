6511dd401cc0f56da70904f8613ebddcac997460
Raul Pino <pinox101@gmail.com>
2024-11-07 00:14:55 -0300

    correct i/o types added

--- oshepherd/api/server.py
+++ oshepherd/api/server.py
@@ -1,8 +1,10 @@
 import signal
 import uvicorn
+from fastapi import FastAPI
+from oshepherd.api.config import ApiConfig
 
 
-def setup_api_server(app, config):
+def setup_api_server(app: FastAPI, config: ApiConfig) -> uvicorn.Server:
     uvicorn_config = uvicorn.Config(
         app=app, host=config.HOST, port=config.PORT, workers=config.WORKERS
     )
