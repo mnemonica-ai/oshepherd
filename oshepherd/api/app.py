d1a8db17d6f8f99cd125d0b6d536d2e4c890c1b0
Raul Pino <pinox101@gmail.com>
2025-02-16 13:48:27 -0300

    network data init and tags endpoint definition in main fast api app

--- oshepherd/api/app.py
+++ oshepherd/api/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from oshepherd.api.config import ApiConfig
 from oshepherd.worker.app import create_celery_app_for_fastapi
+from oshepherd.api.network_data import NetworkData
 from oshepherd.api.generate.routes import load_generate_routes
 from oshepherd.api.embeddings.routes import load_embeddings_routes
 from oshepherd.api.chat.routes import load_chat_routes
+from oshepherd.api.tags.routes import load_tags_routes
 
 
 def setup_api_app(config: ApiConfig) -> FastAPI:
@@ -12,6 +14,10 @@ def setup_api_app(config: ApiConfig) -> FastAPI:
     celery_app = create_celery_app_for_fastapi(config)
     print(" * celery_app ready: ", celery_app)
 
+    network_data = NetworkData(config)
+    print(" * network_data ready: ", network_data)
+    app.network_data = network_data
+
     @app.get("/health")
     async def health():
         return {"status": 200}
@@ -19,5 +25,6 @@ def setup_api_app(config: ApiConfig) -> FastAPI:
     load_generate_routes(app)
     load_embeddings_routes(app)
     load_chat_routes(app)
+    load_tags_routes(app)
 
     return app
