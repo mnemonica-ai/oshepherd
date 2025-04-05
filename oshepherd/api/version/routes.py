c167f1f98a5156e39102e3d3e00d59f4b149a42d
Raul Pino <raul.pino@halborn.com>
2025-04-05 17:06:23 +0200

    version endpoint comments added

--- oshepherd/api/version/routes.py
+++ oshepherd/api/version/routes.py
@@ -1,3 +1,9 @@
+"""
+Version
+API implementation of `GET /api/version` endpoint, getting Ollama version from the first available Oshepherd worker.
+Ollama endpoint reference: https://github.com/ollama/ollama/blob/main/docs/api.md#version
+"""
+
 from fastapi import Request
 from oshepherd.api.utils import streamify_json
 from oshepherd.api.network_data import NetworkData
@@ -11,11 +17,12 @@ def load_version_routes(app):
         print(f" # version request")
 
         ollama_res = network_data.get_version()
+
         status = 200
         if ollama_res.get("error"):
             ollama_res = {
                 "error": "Internal Server Error",
-                "message": "error requesting tags",
+                "message": "error requesting version",
             }
             status = 500
 
