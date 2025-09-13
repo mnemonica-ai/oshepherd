24a991675f52c21d25004bf899df1826c0838f69
Raul Pino <pinox101@gmail.com>
2025-09-13 11:52:39 -0300

    comment removed

--- oshepherd/common/ollama.py
+++ oshepherd/common/ollama.py
@@ -2,15 +2,6 @@ from typing import Any, Union
 
 
 def serialize_ollama_res(response: Any) -> Union[dict, Any]:
-    """
-    Convert response objects to dict for JSON serialization.
-
-    Args:
-        response: The response object to serialize
-
-    Returns:
-        dict: Serializable dictionary representation of the response
-    """
     if hasattr(response, "model_dump"):
         return response.model_dump()
     elif hasattr(response, "__dict__"):
